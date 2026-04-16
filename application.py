514c6245f588d97094361bef1803e03e5d4447ff
Team NeuroNerds <itsninjacoder7@gmail.com>
2026-04-16 22:27:00 +0530

    Update application.py

--- application.py
+++ application.py
@@ -516,16 +516,22 @@ Machine Learning Enthusiast | Aspiring Data Scientist
 
 # Clean Links (Same Font – No Buttons ❌)
 st.sidebar.markdown("""
-<div style="margin-top:14px; font-size:14px;">
-    <a href="https://www.linkedin.com/in/arnav-singh-a87847351" target="_blank"
-       style="color:#4EA1FF; text-decoration:none; margin-right:10px;">
-        🔗 LinkedIn
-    </a>
+<div style="margin-top:14px; font-size:14px; line-height:1.8;">
 
-    <a href="https://github.com/Arnav-Singh-5080" target="_blank"
-       style="color:#4EA1FF; text-decoration:none;">
-        💻 GitHub
-    </a>
+    <div>
+        🔗 <a href="https://www.linkedin.com/in/arnav-singh-a87847351" target="_blank"
+        style="color:#4EA1FF; text-decoration:none;">
+        LinkedIn
+        </a>
+    </div>
+
+    <div style="margin-top:6px;">
+        💻 <a href="https://github.com/Arnav-Singh-5080" target="_blank"
+        style="color:#4EA1FF; text-decoration:none;">
+        GitHub
+        </a>
+    </div>
+
 </div>
 """, unsafe_allow_html=True)
 
